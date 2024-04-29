7153d66bb13ab2f225b3d231f78d6d7a714e6823
Mizerael <dan-acer@outlook.com>
2024-04-29 23:51:08 +0400

    Добавил эндпоинт для вклейки в фон

--- src/cv/src/main.py
+++ src/cv/src/main.py
@@ -3,16 +3,18 @@ from PIL import Image
 from fastapi import FastAPI, UploadFile
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
+import numpy as np
+import cv2
 import io
-from utils import rembg
+from utils import rembg, add_background
 from resources.config import configure_logging, get_config
 
 configure_logging()
 
 app_config = get_config("config/app.json")
-rembg_ctx = get_config(app_config["rembg"]["config"])
+rembg_ctx = get_config(app_config["cv"]["config"])
 
-logger = logging.getLogger("rembg")
+logger = logging.getLogger("cv")
 
 app = FastAPI()
 
@@ -40,3 +42,20 @@ async def remove_background(file: UploadFile):
     content.save(img_buffer, format="PNG")
 
     return StreamingResponse(io.BytesIO(img_buffer.getvalue()), media_type="image/png")
+
+
+@app.post("/paste_to_image")
+async def paste_to_image(background: UploadFile, overlay: UploadFile):
+    background_bytes = await background.read()
+    background_array = np.frombuffer(background_bytes, dtype=np.uint8)
+    background_img = cv2.imdecode(background_array, cv2.IMREAD_UNCHANGED)
+
+    overlay_bytes = await overlay.read()
+    overlay_array = np.frombuffer(overlay_bytes, dtype=np.uint8)
+    overlay_img = cv2.imdecode(overlay_array, cv2.IMREAD_UNCHANGED)
+
+    result_image_bytes = add_background(
+        background_img=background_img, overlay_img=overlay_img
+    )
+
+    return StreamingResponse(io.BytesIO(result_image_bytes), media_type="image/png")
--- src/cv/src/utils.py
+++ src/cv/src/utils.py
@@ -1,4 +1,5 @@
 from PIL import Image, ImageFilter
+import cv2
 
 
 def erode(cycles, image):
@@ -25,3 +26,22 @@ def rembg(img: Image.Image, treshold: int) -> Image.Image:
     blank = img.point(lambda _: 0)
     segmented_img = Image.composite(img, blank, mask)
     return segmented_img
+
+
+def add_background(
+    background_img: cv2.typing.MatLike, overlay_img: cv2.typing.MatLike
+) -> bytes:
+    height, width, _ = overlay_img.shape
+    background_img = cv2.resize(background_img, (width, height))
+    for y in range(height):
+        for x in range(width):
+            overlay_color = overlay_img[y, x, :3]
+            overlay_alpha = overlay_img[y, x, 3] / 255
+            background_color = background_img[y, x]
+            composite_color = (
+                background_color * (1 - overlay_alpha) + overlay_color * overlay_alpha
+            )
+            background_img[y, x] = composite_color
+
+    _, background_img_bytes = cv2.imencode(".png", background_img)
+    return background_img_bytes.tobytes()
